9f199c96b5f1e6b23c3716ba2975093820824dd1
brucebot <bruceboty@gmail.com>
2012-04-24 08:33:06 +0800

    source for brucebot.com

---
+++ instagram/index.php
@@ -0,0 +1,13 @@
+---
+layout: page
+title: "我的instagram"
+date: 2012-03-29
+comments: true
+description: My Photos
+---
+<p>这里显示的是我分享在<a href="http://instagr.am/">instagram</a>照片,我的id是"brucebot",欢迎follow.</p><p>
+也可以在这里<a href="http://brucebot.com/likedphoto/">http://brucebot.com/likedphoto/</a>,看到我赞过的照片.</p>
+
+<?php
+include("./instagram.php");
+?>
---
+++ likedphoto/index.php
@@ -0,0 +1,13 @@
+---
+layout: page
+title: "我喜欢的照片"
+date: 2012-04-08
+comments: true
+description: My Photos
+---
+<p>这里显示的是我在<a href="http://instagr.am">instagram</a>我赞过的照片.</p>
+<p>也可以在这里<a href="http://brucebot.com/instagram/">http://brucebot.com/instagram/</a>,看到我分享在instagram的照片,我的id是"brucebot",欢迎follow.</p>
+
+<?php
+include("./instagram.php");
+?>
